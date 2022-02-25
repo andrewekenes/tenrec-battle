076c39f7bf1400f47b74d0bafe129b3867a27260
Andrew Ekenes <84550532+andrewekenes@users.noreply.github.com>
2022-02-24 18:22:13 -0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Tenrec":return img`
+. . . . . . . . . . . . . . . . 
+. . f 5 f 5 f f 6 6 . . . . . . 
+. f 5 f 5 f 5 6 6 6 6 6 3 . . . 
+. 5 f 5 f 5 f 5 6 6 6 6 3 . . . 
+. . 5 f 5 f f f 6 6 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 3 3 . . . . . . . . 
+. 9 9 9 9 9 3 6 6 . . . . . . . 
+. 9 1 1 1 9 . 6 6 6 f . . . . . 
+. 9 1 2 1 9 . . 6 6 5 f 5 . . . 
+. 9 1 1 1 9 . . f 5 f 5 f 5 . . 
+. 9 1 2 1 9 . . 5 f 5 f 5 f . . 
+. 9 1 2 1 9 . . f 5 f 5 f 5 . . 
+. 9 9 9 9 9 . . . f 5 f 5 f . . 
+. . . . . . . . . . f 5 f 5 . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "Tenrec0":return img`
+. f 5 f 5 f f 6 6 . . . 
+f 5 f 5 f 5 6 6 6 6 6 3 
+5 f 5 f 5 f 5 6 6 6 6 3 
+. 5 f 5 f f f 6 6 . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
